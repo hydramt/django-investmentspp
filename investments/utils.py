7e29d4043d76a6f5120c73756ed7ce45bf2c70c1
hydramt <gatt@hydra.to>
2017-02-16 23:51:11 +0100

    Cleanup

--- investments/utils.py
+++ investments/utils.py
@@ -1,15 +1,5 @@
 def get_links(request):
     temp = request.split('/')[1:-1]
-    #x=1
-    #hrefs = List()
-    #text = []
-    #for i in temp:
-    #  temp2 = []
-    #  for y in i:
-    #    temp2 = temp2.append(y)
-    #  hrefs = hrefs + [ '/'.join(temp2) ]
-    #  text = text.append(i)
-    #links = {'hrefs': hrefs, 'text': text}
     temp2 = ['']
     for x in temp:
       temp2.append('%s/%s' % (temp2[-1], x))
