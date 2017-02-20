f94aa44a5ca6e177531df8a056fb4ccc0a8278c5
hydramt <gatt@hydra.to>
2017-02-20 15:16:11 +0100

    Added working db link mapping

--- investments/utils.py
+++ investments/utils.py
@@ -1,4 +1,5 @@
 from loginbar.models import mapping
+import re
 
 def get_links(request):
     temp = request.split('/')[1:-1]
@@ -7,5 +8,16 @@ def get_links(request):
       temp2.append('%s/%s' % (temp2[-1], x))
     #text = temp
     hrefs = temp2[1:]
-    links = mapping.objects.values('uri','text').filter(uri__in=hrefs)
+    linkdump = mapping.objects.values()
+    links = []
+    for x in hrefs:
+        for y in linkdump:
+          if re.match(y['uri'], x, re.IGNORECASE):
+            temptext = y['text']
+        if temptext:
+          links = links + [{'uri': x, 'text': temptext}]
+          temptext = ''
+        else:
+          links = links + [{'uri': x, 'text': x.split('/')[-1]}]
+    #links = mapping.objects.values('uri','text').filter(uri__in=hrefs)
     return links
